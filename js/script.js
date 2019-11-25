4dfca4d7e35e9aafb0b08eb3debe670eca1287d3
azaazazaa <56756165+azaazazaa@users.noreply.github.com>
2019-11-26 00:21:31 +0300

    обновленная версия для мобилки

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@ const tmp = document.querySelector('.slide-win');
         tmp.addEventListener('click', function() {
             console.log('click!!!');
             const sidebar = document.querySelector('.left-categories');
+            const top = document.querySelector('.header');
+            const bottom = document.querySelector('.bottom__container');
             if(sidebar.classList.contains('show')) {
                 sidebar.classList.remove('show');
+                top.classList.remove('show1');
+                bottom.classList.remove('show2');
             } else {
                 sidebar.classList.add('show');
+                top.classList.add('show1');
+                bottom.classList.add('show2');
             }
             console.log(sidebar);
         })
